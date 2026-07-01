0bc97f3bd794861a31ecfb23dcf1239d8f8d6fde
DeeprajSaha <deeprajsaha401@gmail.com>
2026-07-02 00:20:58 +0530

    Login & Register with localStorage Done and Dashboard created

---
+++ Project9/script.js
@@ -0,0 +1,63 @@
+const login = document.querySelector(".loginForm"); // Login Form
+const register = document.querySelector(".registerForm"); // Register Form
+
+const showRegister = document.querySelector(".showRegister");
+const showLogin = document.querySelector(".showLogin");
+
+const inputUser = document.querySelector("#username");
+const inputPass = document.querySelector("#password");
+
+const regstUser = document.querySelector("#reg-username");
+const regstPass = document.querySelector("#reg-password");
+
+const registerBtn = document.querySelector("#reg-Btn");
+const loginBtn = document.querySelector("#log-Btn");
+
+registerBtn.addEventListener("click", () => {
+    const username = regstUser.value;
+    const password = regstPass.value;
+
+    if (username === "" || password === "") {
+        alert("Please fill all fields!!")
+        return;
+    }
+
+    let user = { username, password };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    alert("Registration successful! Please login.");
+
+    register.classList.add("hidden")
+    login.classList.remove("hidden")
+})
+
+loginBtn.addEventListener("click", () => {
+
+    let userData = JSON.parse(localStorage.getItem("user")) || [];
+    
+    const username = inputUser.value;
+    const password = inputPass.value;
+    if (userData.username === username && userData.password === password) {
+        alert(`Login successful! Welcome back ${username}`);
+    } else {
+        alert("Login Failed Try Agine Latter!!!")
+    }
+})
+
+
+showRegister.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    login.classList.add("hidden");
+    register.classList.remove("hidden");
+})
+
+
+
+showLogin.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    register.classList.add("hidden");
+    login.classList.remove("hidden");
+});
